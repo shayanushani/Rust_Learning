13441e7ba3735aca8ed14eed48540eb31ffca6f5
Shayan Ushani <shayan@Shayans-MacBook-Pro.local>
2020-01-05 17:37:51 -0500

    pushing this

---
+++ hello/target/debug/deps/hello-c10dc46bc4a237f9.d
@@ -0,0 +1,5 @@
+/Users/shayan/Desktop/Rust_Learning/hello/target/debug/deps/hello-c10dc46bc4a237f9: src/main.rs
+
+/Users/shayan/Desktop/Rust_Learning/hello/target/debug/deps/hello-c10dc46bc4a237f9.d: src/main.rs
+
+src/main.rs:
